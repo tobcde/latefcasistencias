4889ea56bde52c39671c93b400803df0edd77054
tobcde <magalottipablo@gmail.com>
2026-04-02 23:21:16 -0300

    Add files via upload

---
+++ sw_1.js
@@ -0,0 +1,25 @@
+var CACHE = 'latefc-v2';
+var ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  // No cachear llamadas al GAS
+  if (e.request.url.indexOf('script.google.com') >= 0) return;
+  if (e.request.url.indexOf('googleapis.com') >= 0) return;
+  e.respondWith(
+    caches.match(e.request).then(function(r){ return r || fetch(e.request); })
+  );
+});
